f2e54317f2a2bef68f36a13ee15888961a0717b0
Shubham Bandagale <sbandagale18@gmail.com>
2021-09-13 23:04:56 +0530

    ADDED: Junit Tests

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -3,8 +3,6 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserRegistrationTest {
     @Test
     void Is_FirstName_Valid_WithMinimumThreeChar_And_StartingLetterCapital() throws UserRegistrationException {
