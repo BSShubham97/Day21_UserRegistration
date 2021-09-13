d92db82e7d142c1f4a80306517791c77f5528a6d
Shubham Bandagale <sbandagale18@gmail.com>
2021-09-13 23:13:06 +0530

    ADDED: Custom Exceptions

--- src/main/java/com/bridgelabz/UserRegistrationException.java
+++ src/main/java/com/bridgelabz/UserRegistrationException.java
@@ -1,4 +1,8 @@
 package com.bridgelabz;
+
+/**
+ * CUSTOM EXCEPTIONS IN CASE OF ANY INVLAID ENTRY
+ */
 public class UserRegistrationException extends Exception {
     enum ExceptionType {
         INVALID_FIRSTNAME,INVALID_LASTNAME,INVALID_EMAIL,INVALID_MOBILE_NUMBER,INVALID_PASSWORD_NUMBER ;
